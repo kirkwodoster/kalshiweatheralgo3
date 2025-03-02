e24074db53f145c0e73aa31d66e6e4babf5c2f3a
kirkwodoster <coreymkirkwood@gmail.com>
2025-03-01 22:41:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,28 +31,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 if __name__ == "__main__":
     
-    xml_url = "https://forecast.weather.gov/MapClick.php?lat=41.7842&lon=-87.7553&FcstType=digitalDWML"
-    timezone =  pytz.timezone("America/Chicago")
-    expected_high_date = scrape_functions.xml_scrape(xml_url, timezone)[0]
-    my_dict = {'current_temp': 24.8, 'market': 'KXHIGHCHI', 'yes_price': 1, 'count': 1, 'balance_min': 100, 'temperatures': [40, 26.6, 26.6, 26.06, 26.6, 26.6, 26.6, 26.6, 26.6, 26.6, 26.6, 26.6, 26.6, 26.6, 26.6, 26.6, 26.06, 26.6, 26.6, 24.8, 26.6, 24.8, 26.6, 26.6, 26.6, 26.6, 26.6, 26.6, 24.8, 24.98, 24.8, 24.8, 24.8, 24.8, 24.8, 24.8, 24.8, 24.8, 24.8], 'lr_length': 5, 'timezone': 'America/Chicago', 'expected_high_date': expected_high_date, 'minutes_from_max': 15}
 
-    x = trade_functions.trade_criteria_met(temperatures=my_dict['temperatures'], lr_length=5, timezone=pytz.timezone("America/Chicago"),expected_high_date=expected_high_date, 
-                                           market='KXHIGHCHI', minutes_from_max=1000, balance_min=100, yes_price=1, count=1)
-    
-    print(expected_high_date)
-    # input = Input()
-    # input.user_input_function()
+    input = Input()
+    input.user_input_function()
 
-    # dict_input = input.user_dict_output()
+    dict_input = input.user_dict_output()
 
-    # driver =  weather_model.initialize_driver()
-    # util_functions.logging_settings()
-    # try:
-    #    weather_model.scrape_dynamic_table(driver, **dict_input)
-    # except KeyboardInterrupt:
-    #     logging.info("Script interrupted by user.")
-    # finally:
-    #     driver.quit()
-    #     logging.info("WebDriver closed.")
+    driver =  weather_model.initialize_driver()
+    util_functions.logging_settings()
+    try:
+       weather_model.scrape_dynamic_table(driver, **dict_input)
+    except KeyboardInterrupt:
+        logging.info("Script interrupted by user.")
+    finally:
+        driver.quit()
+        logging.info("WebDriver closed.")
 
 
